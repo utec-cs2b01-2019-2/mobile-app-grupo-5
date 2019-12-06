0a39e10e58a6a3080dace8987c0235eab34cbb4f
ErickJR <jordan.rosado@utec.edu.pe>
2019-12-06 10:23:33 -0500

    Last
    Triste

--- code/ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/MenuActivity.java
+++ code/ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/MenuActivity.java
@@ -1,7 +1,9 @@
 package cs2b01.utec.chat_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void goToContacts(View view){
+        Intent register = new Intent(this, ContactsActivity.class);
+        startActivity(register);
     }
 }
--- code/ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/RegisterActivity.java
+++ code/ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/RegisterActivity.java
@@ -1,9 +1,28 @@
 package cs2b01.utec.chat_mobile;
 
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.preference.EditTextPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,4 +36,21 @@ public class RegisterActivity extends AppCompatActivity {
         Intent register = new Intent(this, LoginActivity.class);
         startActivity(register);
     }
+
+    public void ActRegister(View view) {
+
+        EditText txtUsername = (EditText) findViewById(R.id.txtUsername);
+        EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+        EditText txtFullname = (EditText) findViewById(R.id.txtFullname);
+        EditText txtNumero = (EditText) findViewById(R.id.txtNumero);
+
+        final String username = txtUsername.getText().toString();
+        final
+        String username = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
+        String fullname = txtFullname.getText().toString();
+        String numero = txtNumero.getText().toString();}
+
+    JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, "http://127.0.0.1:8000/register");
+}
 }
